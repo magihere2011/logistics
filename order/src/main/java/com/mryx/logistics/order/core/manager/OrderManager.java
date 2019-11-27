7e4f2ef053581b13be148d8c2c3308c5dfb1c26f
maqiqi <maqq@missfresh.cn>
2019-11-27 16:21:40 +0800

    Initial commit

---
+++ order/src/main/java/com/mryx/logistics/order/core/manager/OrderManager.java
@@ -0,0 +1,8 @@
+package com.mryx.logistics.order.core.manager;
+
+/**
+ * @author maqiqi
+ * @date 2019-11-26
+ */
+public class OrderManager {
+}
---
+++ order/src/main/java/com/mryx/logistics/order/core/model/Order.java
@@ -0,0 +1,8 @@
+package com.mryx.logistics.order.core.model;
+
+/**
+ * @author maqiqi
+ * @date 2019-11-26
+ */
+public class Order {
+}
